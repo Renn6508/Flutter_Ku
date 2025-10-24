f44b750abe9e80b555d8d71ea13230cf7d4c64bf
Renn6508 <wilhelmina6508@gmail.com>
2025-10-24 10:53:22 +0700

    mengubah main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,58 +6,163 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Loren',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Basic'),
-        backgroundColor: const Color.fromRGBO(39, 83, 202, 1),
-      ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/download (23).jpeg'),
-                    fit: BoxFit.fill,
-                  ),
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(10),
-                  color: const Color.fromRGBO(39, 83, 202, 1),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Wilhelmina Lorenzia Wijaya"),
+          backgroundColor: Colors.green,
+        ),
+        body: ListView(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 250,
+                  height: 50,
+                  color: Colors.white,
+                  child: const Center(
+                    child: Text("BERITA TERBARU", style: TextStyle(fontSize: 15)),
+                  )
                 ),
-                width: 500,
-                height: 500,
-                child: Center(child: Text('kotak 1'),),
+                Container(
+                  width: 250,
+                  height: 50,
+                  color: Colors.white,
+                  child: const Center(
+                    child: Text("PERTANDINGAN HARI INI", style: TextStyle(fontSize: 15)),
+                  )
+                ),
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Container(
+                    width: double.maxFinite,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(image: AssetImage("../assets/berita.png"), fit: BoxFit.cover),
+                      border: Border.all(color: Colors.black)
+                    ),
+                  ),
+                  Container(
+                    width: double.maxFinite,
+                    height: 50,
+                    color: Colors.blue[200],
+                    child: const Center(
+                      child: Text("Potret Krisis Kecelakaan Ringan di Karangsari", style: TextStyle(fontSize: 20)),
+                    ),
+                  ),
+                  Container(
+                    width: double.maxFinite,
+                    height: 50,
+                    padding: EdgeInsets.only(left: 10),
+                    alignment: Alignment.centerLeft,
+                    child: const Text("BERITA TERBARU", style: TextStyle(fontSize: 15)),
+                    color: Colors.purple[200],
+                  ),
+                ],
+              )
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 10, right: 10, bottom: 0, top: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.black),
               ),
+              child: Column(
+                children: [
+                  Row (
+                    children: [
+                      Container(
+                        width: 200,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(image: AssetImage("../assets/fabio.jpg"), fit: BoxFit.cover),
+                          color: Colors.black26,
+                        ),
+                      ),
+                      Container(
+                        width: 278,
+                        height: 150,
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 16),
+                          child: Text("Terbaru, Ilusi Anak Muda di Rembangan Foya-foya, Penggrebekan Polisi Pada Dini Hari ", style: TextStyle(fontSize: 15)),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: double.maxFinite,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border(top: BorderSide(color: Colors.black)),
+                      ),
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 16),
+                      child: Text("6 Januari 2025", style: TextStyle(fontSize: 15),),
+                    ),
+                  ),
+                ],
+              ),
+            ),
 
-            ],
-          ),
-        ],
-      ),
+        
+             Container(
+              margin: const EdgeInsets.only(left: 10, right: 10, bottom: 0, top: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.black),
+              ),
+              child: Column(
+                children: [
+                  Row (
+                    children: [
+                      Container(
+                        width: 200,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(image: AssetImage("../assets/th.jpg"), fit: BoxFit.cover),
+                          color: Colors.black26,
+                        ),
+                      ),
+                      Container(
+                        width: 278,
+                        height: 150,
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 16),
+                          child: Text("FIFA Matchday November Tinggal 20 Hari Lagi, Timnas Indonesia Masih Belum Punya Pelatih,", style: TextStyle(fontSize: 15)),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: double.maxFinite,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border(top: BorderSide(color: Colors.black)),
+                      ),
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 16),
+                      child: Text("6 Januari 2025", style: TextStyle(fontSize: 15),),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
